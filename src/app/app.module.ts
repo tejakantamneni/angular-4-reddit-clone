37149095f7683f2ad26397b163a2c130d2a87681
Teja Kantamneni <teja.kantamneni@gmail.com>
2017-10-21 21:53:35 -0400

    adding updates to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ArticleItemComponent } from './article-item/article-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ArticleItemComponent
   ],
   imports: [
     BrowserModule
