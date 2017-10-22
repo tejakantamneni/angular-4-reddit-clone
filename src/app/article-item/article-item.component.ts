72de4f3c7c57f8002de39765696fb44d12d86237
Teja Kantamneni <teja.kantamneni@gmail.com>
2017-10-21 21:57:18 -0400

    change article component to use article obj instead of generic object

--- src/app/article-item/article-item.component.ts
+++ src/app/article-item/article-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {Article} from '../article';
 
 @Component({
   selector: 'app-article-item',
@@ -7,8 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ArticleItemComponent implements OnInit {
 
-  @Input('article') article: Object;
-  
+  @Input('article') article: Article;
+
   constructor() { }
 
   ngOnInit() {
